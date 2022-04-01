cf4ac73b7d9ce95af60a7e9f4753257b4901e5e3
bonedaddy <boned4ddy@protonmail.com>
2022-04-01 02:47:07 -0700

    add support for atrix pool deposit and withdarw functions

--- atrix/src/lib.rs
+++ atrix/src/lib.rs
@@ -403,5 +403,57 @@ pub mod instructions {
                 data,
             }
         }
+        pub fn new_withdraw_ix(
+            protocol_account: Pubkey,
+            pool_account: Pubkey,
+            pool_coin_token_account: Pubkey,
+            pool_pc_token_account: Pubkey,
+            pool_lp_mint :Pubkey,
+            user_coin_token_account: Pubkey,
+            user_pc_token_account: Pubkey,
+            user_lp_token_account: Pubkey,
+            user_authority: Pubkey,
+            market: Pubkey,
+            open_orders: Pubkey,
+            request_queue: Pubkey,
+            event_queue: Pubkey,
+            bids: Pubkey,
+            asks: Pubkey,
+            serum_coin_vault: Pubkey,
+            serum_pc_vault: Pubkey,
+            serum_vault_signer: Pubkey,
+            withdraw_lp_amount: u64,
+        ) -> Instruction {
+            let mut data = Vec::with_capacity(16);
+            data.extend_from_slice(&GLOBAL_SIGHASHDB.get("withdraw").unwrap()[..]);
+            data.extend_from_slice(&withdraw_lp_amount.to_le_bytes()[..]);
+            Instruction {
+                program_id: addresses::POOL_PROGRAM_ID,
+                accounts: vec![
+                    AccountMeta::new_readonly(protocol_account, false),
+                    AccountMeta::new(pool_account, false),
+                    AccountMeta::new(pool_coin_token_account, false),
+                    AccountMeta::new(pool_pc_token_account, false),
+                    AccountMeta::new(pool_lp_mint, false),
+                    AccountMeta::new(user_coin_token_account, false),
+                    AccountMeta::new(user_pc_token_account, false),
+                    AccountMeta::new(user_lp_token_account, false),
+                    AccountMeta::new_readonly(user_authority, true),
+                    AccountMeta::new(market, false),
+                    AccountMeta::new(open_orders, false),
+                    AccountMeta::new(request_queue, false),
+                    AccountMeta::new(event_queue, false),
+                    AccountMeta::new(bids, false),
+                    AccountMeta::new(asks, false),
+                    AccountMeta::new(serum_coin_vault, false),
+                    AccountMeta::new(serum_pc_vault, false),
+                    AccountMeta::new_readonly(serum_vault_signer, false),
+                    AccountMeta::new_readonly(spl_token::id(), false),
+                    AccountMeta::new_readonly(SERUM_DEX_PROGRAM_ID, false),
+                    AccountMeta::new_readonly(sysvar::rent::id(), false),
+                ],
+                data,
+            }
+        }
     }
 }
