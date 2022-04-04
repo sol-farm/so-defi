12def1ba555e5f818658112d16008e8fd5639c9b
bonedaddy <boned4ddy@protonmail.com>
2022-04-03 22:01:30 -0700

    add so-defi-utils pkg

---
+++ utils/src/lib.rs
@@ -0,0 +1,11 @@
+use std::convert::identity;
+use bytemuck::cast_slice;
+use solana_program::pubkey::Pubkey;
+
+/// returns a public key from a byte slice as 
+/// is used to store public keys in the serum market state account
+pub fn pubkey_from_serum_slice(
+    input: [u64; 4]
+) -> Pubkey {
+    Pubkey::new(cast_slice(&identity(input) as &[_]))
+}
