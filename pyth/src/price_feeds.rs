c5c4622cb2627e3ba4b1f1ace4c4ecf8aee2383e
bonedaddy <boned4ddy@protonmail.com>
2022-09-17 05:04:59 -0700

    wip pyth wrapper crate

---
+++ pyth/src/price_feeds.rs
@@ -0,0 +1,437 @@
+use borsh::{
+    BorshDeserialize,
+    BorshSerialize,
+};
+use crate::price::Price;
+use hex::FromHexError;
+use schemars::JsonSchema;
+use std::fmt;
+use crate::utils;
+
+#[derive(
+    Copy,
+    Clone,
+    Default,
+    PartialEq,
+    Eq,
+    PartialOrd,
+    Ord,
+    Hash,
+    BorshSerialize,
+    BorshDeserialize,
+    serde::Serialize,
+    serde::Deserialize,
+    JsonSchema,
+)]
+#[repr(C)]
+pub struct Identifier(
+    #[serde(with = "hex")]
+    #[schemars(with = "String")]
+    [u8; 32],
+);
+
+impl Identifier {
+    pub fn new(bytes: [u8; 32]) -> Identifier {
+        Identifier(bytes)
+    }
+
+    pub fn to_bytes(&self) -> [u8; 32] {
+        self.0
+    }
+
+    pub fn to_hex(&self) -> String {
+        hex::encode(self.0)
+    }
+
+    pub fn from_hex<T: AsRef<[u8]>>(s: T) -> Result<Identifier, FromHexError> {
+        let mut bytes = [0u8; 32];
+        hex::decode_to_slice(s, &mut bytes)?;
+        Ok(Identifier::new(bytes))
+    }
+}
+
+impl fmt::Debug for Identifier {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "0x{}", self.to_hex())
+    }
+}
+
+impl fmt::Display for Identifier {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "0x{}", self.to_hex())
+    }
+}
+
+impl AsRef<[u8]> for Identifier {
+    fn as_ref(&self) -> &[u8] {
+        &self.0[..]
+    }
+}
+
+/// Consists of 32 bytes and it is currently based on largest Public Key size on various
+/// blockchains.
+pub type PriceIdentifier = Identifier;
+
+/// Consists of 32 bytes and it is currently based on largest Public Key size on various
+/// blockchains.
+pub type ProductIdentifier = Identifier;
+
+/// Unix Timestamp is represented as number of seconds passed since Unix epoch (00:00:00 UTC on 1
+/// Jan 1970). It is a signed integer because it's the standard in Unix systems and allows easier
+/// time difference.
+pub type UnixTimestamp = i64;
+pub type DurationInSeconds = u64;
+
+/// Represents availability status of a price feed.
+#[derive(
+    Copy,
+    Clone,
+    Debug,
+    PartialEq,
+    Eq,
+    BorshSerialize,
+    BorshDeserialize,
+    serde::Serialize,
+    serde::Deserialize,
+    JsonSchema,
+)]
+#[repr(C)]
+pub enum PriceStatus {
+    /// The price feed is not currently updating for an unknown reason.
+    Unknown,
+    /// The price feed is updating as expected.
+    Trading,
+    /// The price feed is not currently updating because trading in the product has been halted.
+    Halted,
+    /// The price feed is not currently updating because an auction is setting the price.
+    Auction,
+}
+
+impl Default for PriceStatus {
+    fn default() -> Self {
+        PriceStatus::Unknown
+    }
+}
+
+/// Represents a current aggregation price from pyth publisher feeds.
+#[derive(
+    Copy,
+    Clone,
+    Debug,
+    Default,
+    PartialEq,
+    Eq,
+    BorshSerialize,
+    BorshDeserialize,
+    serde::Serialize,
+    serde::Deserialize,
+    JsonSchema,
+)]
+#[repr(C)]
+pub struct PriceFeed {
+    /// Unique identifier for this price.
+    pub id:                 PriceIdentifier,
+    /// Status of price (Trading is valid).
+    pub status:             PriceStatus,
+    /// Current price aggregation publish time
+    pub publish_time:       UnixTimestamp,
+    /// Price exponent.
+    pub expo:               i32,
+    /// Maximum number of allowed publishers that can contribute to a price.
+    pub max_num_publishers: u32,
+    /// Number of publishers that made up current aggregate.
+    pub num_publishers:     u32,
+    /// Product account key.
+    pub product_id:         ProductIdentifier,
+    /// The current aggregation price.
+    #[serde(with = "utils::as_string")]
+    #[schemars(with = "String")]
+    price:                  i64,
+    /// Confidence interval around the current aggregation price.
+    #[serde(with = "utils::as_string")]
+    #[schemars(with = "String")]
+    conf:                   u64,
+    /// Exponentially moving average price.
+    #[serde(with = "utils::as_string")]
+    #[schemars(with = "String")]
+    ema_price:              i64,
+    /// Exponentially moving average confidence interval.
+    #[serde(with = "utils::as_string")]
+    #[schemars(with = "String")]
+    ema_conf:               u64,
+    /// Price of previous aggregate with Trading status.
+    #[serde(with = "utils::as_string")]
+    #[schemars(with = "String")]
+    prev_price:             i64,
+    /// Confidence interval of previous aggregate with Trading status.
+    #[serde(with = "utils::as_string")]
+    #[schemars(with = "String")]
+    prev_conf:              u64,
+    /// Publish time of previous aggregate with Trading status.
+    prev_publish_time:      UnixTimestamp,
+}
+
+impl PriceFeed {
+    /// Constructs a new Price Feed
+    #[allow(clippy::too_many_arguments)]
+    pub fn new(
+        id: PriceIdentifier,
+        status: PriceStatus,
+        publish_time: UnixTimestamp,
+        expo: i32,
+        max_num_publishers: u32,
+        num_publishers: u32,
+        product_id: ProductIdentifier,
+        price: i64,
+        conf: u64,
+        ema_price: i64,
+        ema_conf: u64,
+        prev_price: i64,
+        prev_conf: u64,
+        prev_publish_time: UnixTimestamp,
+    ) -> PriceFeed {
+        PriceFeed {
+            id,
+            status,
+            publish_time,
+            expo,
+            max_num_publishers,
+            num_publishers,
+            product_id,
+            price,
+            conf,
+            ema_price,
+            ema_conf,
+            prev_price,
+            prev_conf,
+            prev_publish_time,
+        }
+    }
+
+    /// Get the current price and confidence interval as fixed-point numbers of the form a *
+    /// 10^e.
+    ///
+    /// Returns a struct containing the current price, confidence interval, and the exponent for
+    /// both numbers. Returns `None` if price information is currently unavailable for any
+    /// reason.
+    pub fn get_current_price(&self) -> Option<Price> {
+        if !matches!(self.status, PriceStatus::Trading) {
+            None
+        } else {
+            Some(Price {
+                price: self.price,
+                conf:  self.conf,
+                expo:  self.expo,
+            })
+        }
+    }
+
+    /// Get the "unchecked" current price and confidence interval as fixed-point numbers of the form
+    /// a * 10^e.
+    ///
+    /// Returns a struct containing the current price, confidence interval, and the exponent for
+    /// both numbers. This method returns the price value without checking availability of the
+    /// price. This value might not be valid or updated when the price is not available.
+    /// Please use `get_current_price` where possible.
+    pub fn get_current_price_unchecked(&self) -> Price {
+        Price {
+            price: self.price,
+            conf:  self.conf,
+            expo:  self.expo,
+        }
+    }
+
+    /// Get the exponential moving average price (ema_price) and a confidence interval on the
+    /// result.
+    ///
+    /// Returns `None` if the ema price is currently unavailable.
+    /// At the moment, the confidence interval returned by this method is computed in
+    /// a somewhat questionable way, so we do not recommend using it for high-value applications.
+    pub fn get_ema_price(&self) -> Option<Price> {
+        // This method currently cannot return None, but may do so in the future.
+        Some(Price {
+            price: self.ema_price,
+            conf:  self.ema_conf,
+            expo:  self.expo,
+        })
+    }
+
+    /// Get the "unchecked" exponential moving average price (ema_price) and a confidence interval
+    /// on the result.
+    ///
+    /// Returns the price value without checking availability of the price.
+    /// This value might not be valid or updated when the price is not available.
+    /// Please use `get_ema_price` where possible.
+    ///
+    /// At the moment, the confidence interval returned by this method is computed in
+    /// a somewhat questionable way, so we do not recommend using it for high-value applications.
+    pub fn get_ema_price_unchecked(&self) -> Price {
+        // This method currently cannot return None, but may do so in the future.
+        Price {
+            price: self.ema_price,
+            conf:  self.ema_conf,
+            expo:  self.expo,
+        }
+    }
+
+    /// Get the latest available price, along with the timestamp when it was generated.
+    ///
+    /// This function returns the same price as `get_current_price` in the case where a price was
+    /// available at the time this `PriceFeed` was published (`publish_time`). However, if a
+    /// price was not available at that time, this function returns the price from the latest
+    /// time at which the price was available. The returned price can be from arbitrarily far in
+    /// the past; this function makes no guarantees that the returned price is recent or useful
+    /// for any particular application.
+    ///
+    /// Users of this function should check the returned timestamp to ensure that the returned price
+    /// is sufficiently recent for their application. If you are considering using this
+    /// function, it may be safer / easier to use either `get_current_price` or
+    /// `get_latest_available_price_within_duration`.
+    ///
+    /// Returns a struct containing the latest available price, confidence interval, and the
+    /// exponent for both numbers along with the timestamp when that price was generated.
+    pub fn get_latest_available_price_unchecked(&self) -> (Price, UnixTimestamp) {
+        match self.status {
+            PriceStatus::Trading => (
+                Price {
+                    price: self.price,
+                    conf:  self.conf,
+                    expo:  self.expo,
+                },
+                self.publish_time,
+            ),
+            _ => (
+                Price {
+                    price: self.prev_price,
+                    conf:  self.prev_conf,
+                    expo:  self.expo,
+                },
+                self.prev_publish_time,
+            ),
+        }
+    }
+
+    /// Get the latest price as long as it was updated within `duration` seconds of the
+    /// `current_time`.
+    ///
+    /// This function is a sanity-checked version of `get_latest_available_price_unchecked` which is
+    /// useful in applications that require a sufficiently-recent price. Returns `None` if the
+    /// price wasn't updated sufficiently recently.
+    ///
+    /// Returns a struct containing the latest available price, confidence interval and the exponent
+    /// for both numbers, or `None` if no price update occurred within `duration` seconds of the
+    /// `current_time`.
+    pub fn get_latest_available_price_within_duration(
+        &self,
+        current_time: UnixTimestamp,
+        duration: DurationInSeconds,
+    ) -> Option<Price> {
+        let (price, publish_time) = self.get_latest_available_price_unchecked();
+
+        let time_diff_abs = (publish_time - current_time).abs() as u64;
+
+        if time_diff_abs > duration {
+            return None;
+        }
+
+        Some(price)
+    }
+}
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    pub fn test_ser_then_deser_default() {
+        let price_feed = PriceFeed::default();
+        let ser = serde_json::to_string(&price_feed).unwrap();
+        let deser: PriceFeed = serde_json::from_str(&ser).unwrap();
+        assert_eq!(price_feed, deser);
+    }
+
+    #[test]
+    pub fn test_ser_large_number() {
+        let price_feed = PriceFeed {
+            ema_conf: 1_234_567_000_000_000_789,
+            ..PriceFeed::default()
+        };
+        let price_feed_json = serde_json::to_value(price_feed).unwrap();
+        assert_eq!(
+            price_feed_json["ema_conf"].as_str(),
+            Some("1234567000000000789")
+        );
+    }
+
+    #[test]
+    pub fn test_deser_large_number() {
+        let mut price_feed_json = serde_json::to_value(PriceFeed::default()).unwrap();
+        price_feed_json["price"] = serde_json::Value::String(String::from("1000000000000000123"));
+        let p: PriceFeed = serde_json::from_value(price_feed_json).unwrap();
+        assert_eq!(p.price, 1_000_000_000_000_000_123);
+    }
+
+    #[test]
+    pub fn test_ser_id_length_32_bytes() {
+        let mut price_feed = PriceFeed::default();
+        price_feed.id.0[0] = 106; // 0x6a
+        let price_feed_json = serde_json::to_value(price_feed).unwrap();
+        let id_str = price_feed_json["id"].as_str().unwrap();
+        assert_eq!(id_str.len(), 64);
+        assert_eq!(
+            id_str,
+            "6a00000000000000000000000000000000000000000000000000000000000000"
+        );
+    }
+
+    #[test]
+    pub fn test_deser_invalid_id_length_fails() {
+        let mut price_feed_json = serde_json::to_value(PriceFeed::default()).unwrap();
+        price_feed_json["id"] = serde_json::Value::String(String::from("1234567890"));
+        assert!(serde_json::from_value::<PriceFeed>(price_feed_json).is_err());
+    }
+
+    #[test]
+    pub fn test_identifier_from_hex_ok() {
+        let id = Identifier::from_hex(
+            "0a3f000000000000000000000000000000000000000000000000000000000000",
+        )
+        .unwrap();
+        assert_eq!(id.to_bytes()[0], 10);
+    }
+
+    #[test]
+    pub fn test_identifier_from_hex_invalid_err() {
+        let try_parse_odd = Identifier::from_hex("010"); // odd length
+        assert_eq!(try_parse_odd, Err(FromHexError::OddLength));
+
+        let try_parse_invalid_len = Identifier::from_hex("0a"); // length should be 32 bytes, 64
+        assert_eq!(
+            try_parse_invalid_len,
+            Err(FromHexError::InvalidStringLength)
+        );
+    }
+
+    #[test]
+    pub fn test_identifier_debug_fmt() {
+        let mut id = Identifier::default();
+        id.0[0] = 10;
+
+        let id_str = format!("{:?}", id);
+        assert_eq!(
+            id_str,
+            "0x0a00000000000000000000000000000000000000000000000000000000000000"
+        );
+    }
+
+    #[test]
+    pub fn test_identifier_display_fmt() {
+        let mut id = Identifier::default();
+        id.0[0] = 10;
+
+        let id_str = format!("{}", id);
+        assert_eq!(
+            id_str,
+            "0x0a00000000000000000000000000000000000000000000000000000000000000"
+        );
+    }
+}
